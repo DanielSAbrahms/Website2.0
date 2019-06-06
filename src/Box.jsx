76ee35312fdacdfba6bf5fa9fabe8c1ce0c77d74
Daniel Abrahms <dabrahms@zagmail.gonzaga.edu>
2019-06-05 17:13:33 -0700

    Began working on Animation using GSAP

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,72 +1,88 @@
 import React from "react";
-import $ from "jquery";
+
+import "./assets/infoBox.svg";
+import "./assets/infoIconMeltAnimation.png";
 import "./styles/box.css";
-import velocity from "velocity-animate";
+import { TweenLite, TimelineMax, SteppedEase, TweenMax } from "gsap";
 
 export default class Box extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { isInfoHidden: true };
-        this.showInfo = this.showInfo.bind(this);
-    }
-    showInfo(e) {
-        e.preventDefault();
-        var box = $(this).find(".boxInfoContent");
-        box.css({
-            "background-color": "#ffe",
-            "border-left": "5px solid #ccc"
-        });
+	constructor(props) {
+		super(props);
+		this.infoBox = null;
+		this.infoIcon = null;
+		this.state = {
+			isInfoHidden: true,
+			openInfoBox: new TimelineMax({
+				paused: true
+			}),
+			iconMelt: new TimelineMax({
+				paused: true
+			})
+		};
+		this.toggleInfo = this.toggleInfo.bind(this);
+		this.showInfo = this.showInfo.bind(this);
+		this.closeInfo = this.closeInfo.bind(this);
+	}
+	componentDidMount() {
+		TweenLite.set(this.infoBox, { scaleY: 0, transformOrigin: "50% 0%" });
+		this.state.openInfoBox.to(this.infoBox, 0.5, {
+			scaleY: 1,
+			transformOrigin: "50% 0%"
+		});
+		this.state.openInfoBox.progress(1).progress(0);
 
-        this.setState(state => ({
-            isInfoHidden: !state.isInfoHidden
-        }));
-    }
-    render() {
-        return (
-            <div className="box">
-                <div className="boxTitle">
-                    <div className="boxTitleText">
-                        <h3>{this.props.boxTitle}</h3>
-                    </div>
-                    <div
-                        className="boxTitleInfo"
-                        onClick={this.showInfo}
-                        title={"More on " + this.props.boxTitle}
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            height="25px"
-                            width="25px"
-                            viewBox="0 0 25 25"
-                            fill="white"
-                        >
-                            <g>
-                                <path
-                                    d="M11.812,0C5.289,0,0,5.289,0,11.812s5.289,11.813,11.812,11.813s11.813-5.29,11.813-11.813
-		S18.335,0,11.812,0z M14.271,18.307c-0.608,0.24-1.092,0.422-1.455,0.548c-0.362,0.126-0.783,0.189-1.262,0.189
-		c-0.736,0-1.309-0.18-1.717-0.539s-0.611-0.814-0.611-1.367c0-0.215,0.015-0.435,0.045-0.659c0.031-0.224,0.08-0.476,0.147-0.759
-		l0.761-2.688c0.067-0.258,0.125-0.503,0.171-0.731c0.046-0.23,0.068-0.441,0.068-0.633c0-0.342-0.071-0.582-0.212-0.717
-		c-0.143-0.135-0.412-0.201-0.813-0.201c-0.196,0-0.398,0.029-0.605,0.09c-0.205,0.063-0.383,0.12-0.529,0.176l0.201-0.828
-		c0.498-0.203,0.975-0.377,1.43-0.521c0.455-0.146,0.885-0.218,1.29-0.218c0.731,0,1.295,0.178,1.692,0.53
-		c0.395,0.353,0.594,0.812,0.594,1.376c0,0.117-0.014,0.323-0.041,0.617c-0.027,0.295-0.078,0.564-0.152,0.811l-0.757,2.68
-		c-0.062,0.215-0.117,0.461-0.167,0.736c-0.049,0.275-0.073,0.485-0.073,0.626c0,0.356,0.079,0.599,0.239,0.728
-		c0.158,0.129,0.435,0.194,0.827,0.194c0.185,0,0.392-0.033,0.626-0.097c0.232-0.064,0.4-0.121,0.506-0.17L14.271,18.307z
-		 M14.137,7.429c-0.353,0.328-0.778,0.492-1.275,0.492c-0.496,0-0.924-0.164-1.28-0.492c-0.354-0.328-0.533-0.727-0.533-1.193
-		c0-0.465,0.18-0.865,0.533-1.196c0.356-0.332,0.784-0.497,1.28-0.497c0.497,0,0.923,0.165,1.275,0.497
-		c0.353,0.331,0.53,0.731,0.53,1.196C14.667,6.703,14.49,7.101,14.137,7.429z"
-                                />
-                            </g>
-                        </svg>
-                    </div>
-                </div>
-                <div
-                    className="boxInfoContent"
-                    hidden={this.state.isInfoHidden}
-                >
-                    {this.props.boxInfoContent}
-                </div>
-                <div className="boxContent">{this.props.boxContent}</div>
-            </div>
-        );
-    }
+		TweenLite.set(this.infoBox, { scaleY: 0, transformOrigin: "50% 0%" });
+		this.state.iconMelt.to(this.infoIcon, 0.5, {
+			backgroundPosition: "-1300px",
+			ease: SteppedEase.config(12)
+		});
+		this.state.iconMelt.progress(1).progress(0);
+	}
+	toggleInfo(e) {
+		e.preventDefault();
+		this.state.isInfoHidden ? this.showInfo() : this.closeInfo();
+	}
+	showInfo() {
+		this.setState(state => ({
+			isInfoHidden: false
+		}));
+		this.state.openInfoBox.play();
+		this.state.iconMelt.play();
+	}
+	closeInfo() {
+		this.setState(state => ({
+			isInfoHidden: true
+		}));
+		this.state.openInfoBox.reverse();
+		this.state.iconMelt.reverse();
+	}
+	render() {
+		return (
+			<div className="box">
+				<div className="boxTitle">
+					<div className="boxTitleText">
+						<h3>{this.props.boxTitle}</h3>
+					</div>
+					<div
+						className="boxTitleInfo"
+						onClick={this.toggleInfo}
+						title={"More on " + this.props.boxTitle}
+					>
+						<div
+							className="infoIcon"
+							src="./assets/infoIcon.svg"
+							ref={div => (this.infoIcon = div)}
+						/>
+					</div>
+				</div>
+				<div
+					className="boxInfoContent"
+					ref={div => (this.infoBox = div)}
+				>
+					{this.props.boxInfoContent}
+				</div>
+				<div className="boxContent">{this.props.boxContent}</div>
+			</div>
+		);
+	}
 }
--- src/Main.jsx
+++ src/Main.jsx
@@ -5,51 +5,51 @@ import "./styles/main.css";
 import { Row, Col, Container } from "react-bootstrap";
 
 export default class Main extends React.Component {
-    render() {
-        var colList = [];
-        var text1 =
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec rutrum massa. Duis mollis pellentesque dolor, eget vestibulum magna sodales sed. Fusce finibus, mauris eget dictum pretium, eros arcu tincidunt justo, non mattis est ex et erat. Morbi bibendum laoreet diam, ut consequat odio scelerisque eu. Suspendisse justo purus, lobortis eu arcu sed, tincidunt condimentum lorem. Cras et neque rhoncus, porta leo sed, venenatis diam. Duis tortor ante, imperdiet ac cursus pellentesque, pharetra a nisl. Ut eu accumsan nisl, id posuere sapien. Sed in iaculis enim. Phasellus turpis tellus, pellentesque eget dolor quis, finibus egestas dui. Suspendisse potenti. Aenean cursus imperdiet elementum. Nunc ultrices ullamcorper eleifend. Nunc posuere odio vel tincidunt tristique.";
-        var text2 =
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec rutrum massa. Duis mollis pellentesque dolor, eget vestibulum magna sodales sed. Fusce finibus, mauris eget dictum pretium, eros arcu tincidunt justo, non mattis est ex et erat. Morbi bibendum laoreet diam, ut consequat odio scelerisque eu. Suspendisse justo purus, lobortis eu arcu sed, tincidunt condimentum lorem. Cras et neque rhoncus, porta leo sed, venenatis diam. Duis tortor ante, imperdiet ac cursus pellentesque, pharetra a nisl. Ut eu accumsan nisl, id posuere sapien. Sed in iaculis enim. Phasellus turpis tellus, pellentesque eget dolor quis, finibus egestas dui. Suspendisse potenti. Aenean cursus imperdiet elementum.";
-        var text3 =
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, mauris eget dictum pretium, eros arcu tincidunt justo, non mattis est ex et erat. Morbi bibendum laoreet diam, ut consequat odio scelerisque eu. Suspendisse justo purus, lobortis eu arcu sed, tincidunt condimentum lorem. Cras et neque rhoncus, porta leo sed, venenatis diam. Duis tortor ante, imperdiet ac cursus pellentesque, pharetra a nisl. Ut eu accumsan nisl, id posuere sapien. Sed in iaculis enim. Phasellus turpis tellus, pellentesque eget dolor quis, finibus egestas dui. Suspendisse potenti.";
-        const boxCollection = [
-            { title: "Box1", content: <p>{text1}</p> },
-            { title: "Box2", content: <p>{text2}</p> },
-            { title: "Box3", content: <p>{text3}</p> },
-            { title: "Box4", content: <p>{text1}</p> },
-            { title: "Box5", content: <p>{text1}</p> },
-            { title: "Box6", content: <p>{text2}</p> },
-            { title: "Box7", content: <p>{text3}</p> },
-            { title: "Box8", content: <p>{text3}</p> }
-        ];
-        for (var index = 0; index < boxCollection.length; index++) {
-            colList.push(boxCollection[index]);
-        }
-        return (
-            <div className="main">
-                <Title text={"Daniel S. Abrahms"} />
+	render() {
+		var colList = [];
+		var text1 =
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec rutrum massa. Duis mollis pellentesque dolor, eget vestibulum magna sodales sed. Fusce finibus, mauris eget dictum pretium, eros arcu tincidunt justo, non mattis est ex et erat. Morbi bibendum laoreet diam, ut consequat odio scelerisque eu. Suspendisse justo purus, lobortis eu arcu sed, tincidunt condimentum lorem. Cras et neque rhoncus, porta leo sed, venenatis diam. Duis tortor ante, imperdiet ac cursus pellentesque, pharetra a nisl. Ut eu accumsan nisl, id posuere sapien. Sed in iaculis enim. Phasellus turpis tellus, pellentesque eget dolor quis, finibus egestas dui. Suspendisse potenti. Aenean cursus imperdiet elementum. Nunc ultrices ullamcorper eleifend. Nunc posuere odio vel tincidunt tristique.";
+		var text2 =
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec rutrum massa. Duis mollis pellentesque dolor, eget vestibulum magna sodales sed. Fusce finibus, mauris eget dictum pretium, eros arcu tincidunt justo, non mattis est ex et erat. Morbi bibendum laoreet diam, ut consequat odio scelerisque eu. Suspendisse justo purus, lobortis eu arcu sed, tincidunt condimentum lorem. Cras et neque rhoncus, porta leo sed, venenatis diam. Duis tortor ante, imperdiet ac cursus pellentesque, pharetra a nisl. Ut eu accumsan nisl, id posuere sapien. Sed in iaculis enim. Phasellus turpis tellus, pellentesque eget dolor quis, finibus egestas dui. Suspendisse potenti. Aenean cursus imperdiet elementum.";
+		var text3 =
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, mauris eget dictum pretium, eros arcu tincidunt justo, non mattis est ex et erat. Morbi bibendum laoreet diam, ut consequat odio scelerisque eu. Suspendisse justo purus, lobortis eu arcu sed, tincidunt condimentum lorem. Cras et neque rhoncus, porta leo sed, venenatis diam. Duis tortor ante, imperdiet ac cursus pellentesque, pharetra a nisl. Ut eu accumsan nisl, id posuere sapien. Sed in iaculis enim. Phasellus turpis tellus, pellentesque eget dolor quis, finibus egestas dui. Suspendisse potenti.";
+		const boxCollection = [
+			{ title: "Box1", content: <p>{text1}</p> },
+			{ title: "Box2", content: <p>{text2}</p> },
+			{ title: "Box3", content: <p>{text3}</p> },
+			{ title: "Box4", content: <p>{text1}</p> },
+			{ title: "Box5", content: <p>{text1}</p> },
+			{ title: "Box6", content: <p>{text2}</p> },
+			{ title: "Box7", content: <p>{text3}</p> },
+			{ title: "Box8", content: <p>{text3}</p> }
+		];
+		for (var index = 0; index < boxCollection.length; index++) {
+			colList.push(boxCollection[index]);
+		}
+		return (
+			<div className="main">
+				<Title text={"Daniel S. Abrahms"} />
 
-                <Container className="columnBox">
-                    <Row>
-                        <Col xs={12} md={6} xl={4}>
-                            <Column
-                                boxes={colList.filter((a, i) => i % 3 === 0)}
-                            />
-                        </Col>
-                        <Col xs={12} md={6} xl={4}>
-                            <Column
-                                boxes={colList.filter((a, i) => i % 3 === 1)}
-                            />
-                        </Col>
-                        <Col xs={12} md={6} xl={4}>
-                            <Column
-                                boxes={colList.filter((a, i) => i % 3 === 2)}
-                            />
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        );
-    }
+				<Container className="columnBox">
+					<Row>
+						<Col xs={12} md={6} xl={4}>
+							<Column
+								boxes={colList.filter((a, i) => i % 3 === 0)}
+							/>
+						</Col>
+						<Col xs={12} md={6} xl={4}>
+							<Column
+								boxes={colList.filter((a, i) => i % 3 === 1)}
+							/>
+						</Col>
+						<Col xs={12} md={6} xl={4}>
+							<Column
+								boxes={colList.filter((a, i) => i % 3 === 2)}
+							/>
+						</Col>
+					</Row>
+				</Container>
+			</div>
+		);
+	}
 }
